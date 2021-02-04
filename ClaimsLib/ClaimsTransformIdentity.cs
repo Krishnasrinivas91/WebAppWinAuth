1e5a99c22ef28a36ab82721395ce1a0ec55e20e2
krishnasrinivas91 <krishnasrinivas91@gmail.com>
2021-02-04 11:45:57 +0530

    Add project files.

---
+++ ClaimsLib/ClaimsTransformIdentity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ClaimsLib
+{
+    public class ClaimsTransformIdentity : IClaimsTransformation
+    {
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            var claiminfo = (ClaimsIdentity)principal.Identity;
+            string[] Name = claiminfo.Name.Split("\\");
+            var role = "user"; // Get User roles either from login identity or from DB. As of now Hardcoded
+            var c = new Claim(claiminfo.RoleClaimType, role);
+            claiminfo.AddClaim(c);
+            return Task.FromResult(principal);
+        }
+    }
+}
